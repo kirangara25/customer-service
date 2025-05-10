948549aba90da71812421faa1cf70de66e5fd780
kirangara25 <kirangara25@hotmail.com>
2025-05-10 00:16:28 -0500

    Initial commit - 2nd commit

--- src/main/java/com/insurance/customer/service/CustomerServiceApplication.java
+++ src/main/java/com/insurance/customer/service/CustomerServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CustomerServiceApplication.class, args);
+		
 	}
 }
